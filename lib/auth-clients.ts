b660efabe474f2988c8630b04bd0934b49a4367e
Ivano PETTY <96083465+Ivano-P@users.noreply.github.com>
2026-04-07 22:50:18 +0200

    added better auth and prisma

---
+++ lib/auth-clients.ts
@@ -0,0 +1,8 @@
+import { createAuthClient } from "better-auth/react"
+export const authClient = createAuthClient({
+    /** The base URL of the server (optional if you're using the same domain) */
+    baseURL: process.env.BETTER_AUTH_URL
+})
+
+// Export des hooks pratiques pour l'UI
+export const { signIn, signUp, useSession, signOut } = authClient;
---
+++ lib/auth.ts
@@ -0,0 +1,18 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql", // changed from sqlite to match your schema
+    }),
+    emailAndPassword: {
+        enabled: true,
+    },
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        },
+    },
+});
